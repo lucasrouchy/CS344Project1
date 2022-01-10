640e71963d88aa72bd2a366e936e355273f2443a
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-01-09 18:15:25 -0800

    Cleared up geturl just have to make sure you are calling the .sh file on the command line

---
+++ bigfiles2.sh
@@ -0,0 +1,4 @@
+#!/usr/bin
+find $1 -type f -exec ls -s {} + | sort -n -r | head -7; 
+
+#using the man command on find and sort helped me a lot with understanding all the ways you can use them. These options of the command made it so I did not have the pipe a bunch of commands together.
