52e8d279d13d8fa898663da4a22b518ff8ffa19f
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-01-09 16:51:14 -0800

    changed the syntax to work when called as a shell. Tried to get the shebang to work as intended but its not so its finding the top 7 biggest files within this directory

--- bigfiles.sh
+++ bigfiles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-find . -type f -exec ls -s {} | sort -n -r | head -7
+#!/usr/bin
+find . -type f -exec ls -s {} + | sort -n -r | head -7; 
 
 #using the man command on find and sort helped me a lot with understanding all the ways you can use them. These options of the command made it so I did not have the pipe a bunch of commands together.
